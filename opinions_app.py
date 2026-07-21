b946701df93d255781cfe4ff13ab6132503407af
mihail mihailovich <mixmix19892@gmail.com>
2026-07-22 00:06:55 +0300

    Первые шаги в-на фласк!

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,40 @@
-from flask import Flask
+# what_to_watch/opinions_app.py
+
+from datetime import datetime
+from random import randrange
+
+# Импортировать функцию render_template().
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db = SQLAlchemy(app)
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 @app.route('/')
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    quantity = Opinion.query.count()
+    if not quantity:
+        return 'В базе данных мнений о фильмах нет.'
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    # Вот он — возврат функции.
+    return render_template('index.html', opinion=opinion)
+
+
+@app.route('/add')
+def add_opinion_view():
+    return 'Страница в разработке!'
+
 
 if __name__ == '__main__':
     app.run()
